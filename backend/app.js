79512efdf4e390bb82b24d5238cda0f83022ce06
Ajay <ajayshakya7376@gmail.com>
2025-05-05 23:18:30 +0530

    feat : adding more origin

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ import connectDB from "./db/db.js";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/project.routes.js";
-import portfolioRoutes from './routes/portfolioRoutes.js'
-import aiRoutes from './routes/ai.routes.js'
+import portfolioRoutes from "./routes/portfolioRoutes.js";
+import aiRoutes from "./routes/ai.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -19,7 +19,13 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://wechat-teal.vercel.app","http://localhost:5000","https://ajaykumarshakya.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://wechat-teal.vercel.app",
+      "http://localhost:5000",
+      "https://ajaykumarshakya.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
@@ -29,7 +35,6 @@ app.use("/auth", authRoutes);
 app.use("/project", projectRoutes);
 app.use("/portfolio", portfolioRoutes);
 app.use("/ai", aiRoutes);
-
 
 app.get("/", (req, res) => {
   res.send(` WeChat backend is running`);
